4287288a9664a1134c5b4b063db322fec9053bf0
Ryan Marvin <marvinryan@ymail.com>
2020-05-05 02:33:05 +0300

    Add shit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+class App extends React.Component {
+    render() {
+        debugger
+        return (<div>
+            <h1>Hello WOOWOWOWOWOW</h1>
+        </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
+document.body.style.border = "5px solid red"
+
