cd376358245905ec5cd15c7953d56918420bbc95
Ryan Marvin <marvinryan@ymail.com>
2020-06-07 03:19:05 +0300

    Add mounting to each issue comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react"
+import * as ReactDOM from "react-dom"
 
-import App from './App';
+import App from "./App"
 
-document.body.style.border = "5px solid red"
+function getIssueCommentDivs(): NodeList {
+    const commentDivs = document.querySelectorAll(
+        "div.timeline-comment-group[id^=issuecomment]"
+    )
+    return commentDivs
+}
 
-
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+getIssueCommentDivs().forEach((commentDiv: Element) => {
+    const voiceNoteContainerID = `${commentDiv.id}-voice-note-container`
+    const voiceNoteContainer = `
+            <voiceNoteContainer class="d-block">
+                <td class="d-block comment-body js-comment-body gvn-container" id="${voiceNoteContainerID}">
+                </td>
+            </tr>
+            `
+    commentDiv
+        .querySelector("task-lists table")
+        .insertAdjacentHTML("beforeend", voiceNoteContainer)
+    ReactDOM.render(<App />, document.getElementById(voiceNoteContainerID))
+})
