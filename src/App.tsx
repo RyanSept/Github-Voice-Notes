0e0b0d3bcb84d8dc3f82037901a7e4513fc35ea9
Ryan Marvin <marvinryan@ymail.com>
2021-04-11 00:11:32 +0300

    Introduce recording FSM and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,100 @@
 import * as React from "react"
+import * as StateMachine from "javascript-state-machine"
 
 const css = require("./index.css")
 
-class App extends React.Component {
+interface AppProps {}
+interface AppState {
+    recordingFSMState: string
+}
+
+class App extends React.Component<AppProps, AppState> {
+    recordingFSM: StateMachine
+
+    constructor(props) {
+        super(props)
+        this.recordingFSM = new StateMachine({
+            init: "off",
+            transitions: [
+                { name: "start", from: "off", to: "recording" },
+                { name: "accept", from: "recording", to: "ended" },
+                { name: "timeEnd", from: "recording", to: "ended" },
+                { name: "cancel", from: "recording", to: "canceled" },
+                { name: "reset", from: "*", to: "off" },
+            ],
+            methods: {
+                onStart: () => {
+                    console.log("Starting recording ...")
+                },
+                onAccept: () => {
+                    console.log("Ended recording due to user ...")
+                },
+                onTimeEnd: () => {
+                    console.log("Ended recording due to time end ...")
+                },
+                onCancel: () => {
+                    console.log("Canceled recording ...")
+                },
+                onEnterState: (lifecycle) => {
+                    this.setState({ recordingFSMState: lifecycle.to })
+                },
+            },
+        })
+        this.state = { recordingFSMState: this.recordingFSM.state }
+    }
+
     render() {
         const gvnIcon = chrome.runtime.getURL("static/gvn-icon-128.png")
-        console.log(css.voiceRecordButton)
-        console.log("ashisogi")
+        console.log("Embeded GVN")
         return (
             <>
                 <div
-                    className={css.voiceRecordButton}
+                    className={css.voiceRecordStartButton}
+                    onClick={() => {
+                        this.recordingFSM.start()
+                        console.log(this.recordingFSM.state)
+                    }}
                     style={{
                         backgroundImage: `url(${gvnIcon})`,
                     }}
-                >
-                    {/* <img src={gvnIcon} className={css.voiceRecordButtonIcon}/> */}
-                </div>
+                ></div>
+                {this.state.recordingFSMState === "recording" && (
+                    <div className={`${css.voiceRecordControls} d-flex pl-1`}>
+                        {/* octicon check-circle-fill */}
+                        <svg
+                            className="octicon color-icon-success mr-1"
+                            onClick={() => {
+                                this.recordingFSM.accept()
+                            }}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            width="20"
+                            height="20"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M8 16A8 8 0 108 0a8 8 0 000 16zm3.78-9.72a.75.75 0 00-1.06-1.06L6.75 9.19 5.28 7.72a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l4.5-4.5z"
+                            ></path>
+                        </svg>
+                        <p>00:59</p>
+                        {/* octicon check-circle-fill */}
+                        <svg
+                            className="octicon color-icon-danger ml-1"
+                            onClick={() => {
+                                this.recordingFSM.cancel()
+                            }}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            width="20"
+                            height="20"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M2.343 13.657A8 8 0 1113.657 2.343 8 8 0 012.343 13.657zM6.03 4.97a.75.75 0 00-1.06 1.06L6.94 8 4.97 9.97a.75.75 0 101.06 1.06L8 9.06l1.97 1.97a.75.75 0 101.06-1.06L9.06 8l1.97-1.97a.75.75 0 10-1.06-1.06L8 6.94 6.03 4.97z"
+                            ></path>
+                        </svg>
+                    </div>
+                )}
             </>
         )
     }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom"
 
 import App from "./App"
 
+const css = require("./index.css")
+
 function getCommentFileAttachments(): NodeList {
     const commentFileAttachments = document.querySelectorAll(
         "file-attachment.js-upload-markdown-image"
@@ -30,24 +32,25 @@ function insertNewVoiceNoteButton() {
     newCommentFormActions.insertAdjacentHTML("afterbegin", newVoiceNoteButton)
 }
 
+function embedGVNOnFileAttachments(commentFileAttachment: Element) {
+    const voiceRecordControlsContainerID = `voice-record-controls-container-${commentFileAttachment.attributes["input"].value}`
+    const voiceRecordButtonContainer = `
+            <div id="${voiceRecordControlsContainerID}" class="${css.voiceRecordControlsContainer} pt-1">
+            </div>
+            `
+    commentFileAttachment.parentElement.insertAdjacentHTML(
+        "beforeend",
+        voiceRecordButtonContainer
+    )
+    ReactDOM.render(
+        <App />,
+        document.getElementById(voiceRecordControlsContainerID)
+    )
+}
+
 function setup() {
     // Render in file comments too
-    getCommentFileAttachments().forEach((commentFileAttachment: Element) => {
-        const voiceRecordButtonContainerID = `voice-record-button-container-${commentFileAttachment.attributes["input"].value}`
-        const voiceRecordButtonContainer = `
-                <div id="${voiceRecordButtonContainerID}">
-                90h8g7f6d8g9hj
-                </div>
-                `
-        commentFileAttachment.parentElement.insertAdjacentHTML(
-            "beforeend",
-            voiceRecordButtonContainer
-        )
-        ReactDOM.render(
-            <App />,
-            document.getElementById(voiceRecordButtonContainerID)
-        )
-    })
+    getCommentFileAttachments().forEach(embedGVNOnFileAttachments)
 
     // insertNewVoiceNoteButton()
 }
@@ -58,46 +61,19 @@ dataTransfer = new DataTransfer()
 let fil = new File(['file:///Users/ryanmarvin/Downloads/weather-api-dev-1583096221/pylint/test/functional/access_to_protected_members.txt'], 'testFile.txt', {type: 'text/plain'})
 dataTransfer.items.add(fil)
 fileAttachment.attach(dataTransfer)
-
-Style
-{
-    width: 40px;
-    height: 40px;
-    font-size: 25px;
-    position: absolute;
-    right: 0;
-    z-index: 1;
-    text-align: center;
-    background: white;
-    border-radius: 40px;
-    border: 1px solid lightgrey;
-    box-shadow: 4px 7px 10px -6px;
-    top: 22%;
-}
 */
 setup()
 
 const observer = new MutationObserver(function (mutations: MutationRecord[]) {
     mutations.forEach(function (mutation: MutationRecord) {
         for (let addedNode of mutation.addedNodes)
-            if (
-                addedNode instanceof Element &&
-                addedNode.classList.contains("new-discussion-timeline")
-            ) {
+            if (addedNode instanceof Element) {
                 const commentFileAttachments = addedNode.querySelectorAll(
                     "file-attachment.js-upload-markdown-image"
                 )
+                commentFileAttachments.forEach(embedGVNOnFileAttachments)
             }
     })
 })
 observer.observe(document, { childList: true, subtree: true })
 console.log("Registered shit")
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // listen for messages sent from background.js
-    if (request.message === "hello!") {
-        console.log("CHUBBLEGUM")
-        // DOESN'T ACTUALLY SETUP SINCE DOM HASN'T LOADED YET. MUTATION OBSERVER MIGHT BE MORE RELIABLE
-        setup()
-    }
-})
